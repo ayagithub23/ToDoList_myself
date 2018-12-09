fa101171cea93fac464722db3b24589a40c49ffc
ayagithub23 <neptune32ac@gmail.com>
2018-12-09 19:50:13 +0800

    2018/12/04 回家作業

---
+++ app.js
@@ -0,0 +1,9 @@
+$("#inputArea button").on("click",function(){
+    alert("點我Button");
+});
+$("#inputArea input[type='button']").on("click",function(){
+    alert("點我InputButton");
+});
+$("#inputArea input[type='submit']").on("click",function(){
+    alert("點我InputSubmit");
+});
